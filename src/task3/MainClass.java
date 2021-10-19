8420f301dac456f109c7d611cd820c089ba3a020
kuanysh <kuanysh.bekturganov@aitudala.kz>
2021-10-19 19:19:47 +0600

    Task 3

---
+++ src/task3/MainClass.java
@@ -0,0 +1,15 @@
+package task3;
+
+public class MainClass {
+
+    /*
+    Сделать в классе MainClass приватное поле класса,
+    которое равно строке “Hello, world” (назвать: class_string),
+    и публичный метод (назвать: getClassString), который возвращает строку.
+    */
+    private String class_string = "Hello, world";
+
+    public String getClassString() {
+        return class_string;
+    }
+}
